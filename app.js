badda0e824f145b5f6d95b57a7fdaf8f5f5f6694
Jonathan Mutakura (jtm813) <jmutakura@gmail.com>
2021-10-21 21:32:39 -0600

    Did the following: Finished the basic structure of the site Made a few changes onto the server Made an .env file

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
-
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 //register view engine
 app.set('view engine', 'ejs');
 
@@ -14,5 +17,6 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
 console.log('Server Running ...');
